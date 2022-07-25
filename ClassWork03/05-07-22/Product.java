e2c3995fe1586f10384f5e822a19c63a2e936266
islamofficial5 <95912179+islamofficial5@users.noreply.github.com>
2022-07-25 13:49:58 +0530

    Create Product.java

---
+++ ClassWork03/05-07-22/Product.java
@@ -0,0 +1,12 @@
+//Write a program to print product of two digits.
+public class Product
+{
+	
+	public static void main(String[] args)
+	{
+		int x=5;
+		int y=4;
+		int sum= x*y;
+		System.out.println("The Product of two digits are=" +sum);
+	}
+}
