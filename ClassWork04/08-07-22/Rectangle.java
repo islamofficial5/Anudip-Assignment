7b7bde022298fa14c692617858aef5e26ea4f556
islamofficial5 <95912179+islamofficial5@users.noreply.github.com>
2022-07-25 13:51:14 +0530

    Create Rectangle.java

---
+++ ClassWork04/08-07-22/Rectangle.java
@@ -0,0 +1,11 @@
+//Write a program to find out length and breadthof a rectangle.
+public class Ractangle
+{
+	public static void main(String[] args)
+	{
+	int length =5;
+	int breadth =10;
+	int area = length*breadth;
+	System.out.println("Area of Ractangle=" +area);
+	}
+}
