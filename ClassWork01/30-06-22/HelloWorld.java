68f40272da716ead2309181fbb8e726fafcb8c16
islamofficial5 <95912179+islamofficial5@users.noreply.github.com>
2022-07-25 13:43:08 +0530

    Create HelloWorld.java

---
+++ ClassWork01/30-06-22/HelloWorld.java
@@ -0,0 +1,12 @@
+public class Sample1
+{
+	public void meth1()
+	{
+			System.out.println("Hello World\n I am Islam");
+	}
+	public static void main(String[] args)
+	{
+		Sample1 aobj = new Sample1();
+		aobj.meth1();
+	}
+}
