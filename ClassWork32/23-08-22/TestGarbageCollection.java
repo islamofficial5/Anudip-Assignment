f4f84192a038334ff8d2e2fc4b2e250f5bb2fcad
islamofficial5 <95912179+islamofficial5@users.noreply.github.com>
2022-08-31 14:05:28 +0530

    Add files via upload

---
+++ ClassWork32/23-08-22/TestGarbageCollection.java
@@ -0,0 +1,19 @@
+package multithreading;
+
+public class TestGarbageCollection
+{
+	public void finalize()
+	{
+		System.out.println("all objects are collected for garbage collection");
+	}
+	
+	public static void main(String[] args) 
+	{
+		TestGarbageCollection t1 = new TestGarbageCollection();
+		TestGarbageCollection t2 = new TestGarbageCollection();
+		t1=null;
+		t2=null;
+		System.gc();
+		System.out.println("all are unreferenced");
+	}
+}
